9f5556169429ebbc73da55db0e0ac713143061a8
joesimmons <joesimmons@labgrownmusic.com>
2022-09-01 16:47:11 +0100

    Wwise functionality added
     basic parameter map achieved based on distance of robot to centre of rad sphere

--- Assets/Scripts/WwiseData.cs
+++ Assets/Scripts/WwiseData.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 //template script for accessing environment data
 public class WwiseData : MonoBehaviour
 {
     public GetEnvironmentData avatarData;//add one of these for each thing that interacts with the data and assign in the inspector 
     public GetEnvironmentData robot1Data;//drag the objects from the hierarchy, the objects must have a GetEnvironmentData script on them
+
+    public AK.Wwise.Event RadPlay;
+    public AK.Wwise.RTPC RadLevel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        RadPlay.Post(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {        
-        float test2 = avatarData.data["rad"][0];
+        float robot1rad = robot1Data.data["rad"][0];
+        float robot1temp = robot1Data.data["temp"][0];
+        float robot1gas = robot1Data.data["gas"][0];
+
+        RadLevel.SetValue(this.gameObject, robot1rad);
+
     }
 }
+
+//need a public GetEnvironmentData and declaration name
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,47 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GAS_PLAY = 609952641U;
+        static const AkUniqueID GAS_STOP = 2929912663U;
+        static const AkUniqueID RAD_PLAY = 245047015U;
+        static const AkUniqueID RAD_STOP = 3233679413U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID RADLEVEL = 994322176U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ALISON_SOUNDSET = 785106775U;
+        static const AkUniqueID JOE_SOUNDSET = 382517303U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
