58c4705511ce4227cd07f0343e738e120e9a2cbd
joesimmons <joesimmons@labgrownmusic.com>
2022-09-02 17:22:26 +0100

    Wwise RTPCs and reverb zones
    3 data types now working with RTPCs. test level built. reverb zones added. visual tweaking

--- Assets/Scripts/WwiseData.cs
+++ Assets/Scripts/WwiseData.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class WwiseData : MonoBehaviour
 {
     public GetEnvironmentData avatarData;//add one of these for each thing that interacts with the data and assign in the inspector 
-    public GetEnvironmentData robot1Data;//drag the objects from the hierarchy, the objects must have a GetEnvironmentData script on them
+    public GetEnvironmentData robotData;//drag the objects from the hierarchy, the objects must have a GetEnvironmentData script on them
 
     public AK.Wwise.Event RadPlay;
     public AK.Wwise.RTPC RadLevel;
@@ -20,11 +20,11 @@ public class WwiseData : MonoBehaviour
     // Update is called once per frame
     void Update()
     {        
-        float robot1rad = robot1Data.data["rad"][0];
-        float robot1temp = robot1Data.data["temp"][0];
-        float robot1gas = robot1Data.data["gas"][0];
+        float robotRad = robotData.data["rad"][0];
+        float robotTemp = robotData.data["temp"][0];
+        float robotGas = robotData.data["gas"][0];
 
-        RadLevel.SetValue(this.gameObject, robot1rad);
+        RadLevel.SetValue(gameObject, robotRad);
 
     }
 }
