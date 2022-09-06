7cfcef7a776f70bbbf85f4363385727d030dd6e2
PaulBremner <king.speen@gmail.com>
2022-09-06 15:44:05 +0100

    Revert "Wwise RTPCs and reverb zones"
    This reverts commit 58c4705511ce4227cd07f0343e738e120e9a2cbd.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -17,15 +17,11 @@ namespace AK
         static const AkUniqueID GAS_STOP = 2929912663U;
         static const AkUniqueID RAD_PLAY = 245047015U;
         static const AkUniqueID RAD_STOP = 3233679413U;
-        static const AkUniqueID TEMP_PLAY = 2832289476U;
-        static const AkUniqueID TEMP_STOP = 1309769406U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
-        static const AkUniqueID GASLEVEL = 423230026U;
         static const AkUniqueID RADLEVEL = 994322176U;
-        static const AkUniqueID TEMPLEVEL = 2582419067U;
     } // namespace GAME_PARAMETERS
 
     namespace BANKS
@@ -38,14 +34,7 @@ namespace AK
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
-        static const AkUniqueID REVERBS = 3545700988U;
     } // namespace BUSSES
-
-    namespace AUX_BUSSES
-    {
-        static const AkUniqueID NUCLEARWAREHOUSEVERB = 2932930741U;
-        static const AkUniqueID WAREHOUSERECESSVERB = 1362633108U;
-    } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES
     {
